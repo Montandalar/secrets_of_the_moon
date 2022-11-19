bba7eb9bcf2628138ee959d24a1a2c270f357360
Blockhead <jbis1337@hotmail.com>
2022-11-19 19:59:59 +1100

    Add aluminium, portholes and worklights

--- mods/sotm_nodes/init.lua
+++ mods/sotm_nodes/init.lua
@@ -1,4 +1,18 @@
+-- Natural Environment
+minetest.register_node("sotm_nodes:vacuum", {
+    description = "Vacuum",
+    drawtype = "airlike",
+    paramtype = "light",
+    sunlight_propagates = true,
+    walkable = false,
+    pointable = false,
+    diggable = false,
+    buildable_to = true,
+    floodable = true,
+})
+
 minetest.register_node("sotm_nodes:moonrock", {
+    description = "Moon Rock",
     drawtype = "normal",
     tiles = {"sotm_moonrock.png"},
     groups = {cracky=1},
@@ -8,17 +22,42 @@ minetest.register_node("sotm_nodes:moonrock", {
     },
 })
 
-minetest.register_node("sotm_nodes:vacuum", {
-    drawtype = "airlike",
-    description = "Vacuum",
-    paramtype = "light",
-    sunlight_propagates = true,
-    walkable = false,
-    pointable = false,
-    diggable = false,
-    buildable_to = true,
-    floodable = true,
+-- Built environment
+minetest.register_node("sotm_nodes:al2219", {
+    description = "Aluminium Alloy 2219",
+    drawtype = "normal",
+    tiles = {"sotm_al2219.png"},
+    groups = {metal=1, snappy=1},
 })
+
+minetest.register_node("sotm_nodes:porthole", {
+    description = "Porthole",
+    drawtype = "glasslike",
+    tiles = {"sotm_porthole.png"},
+    use_texture_alpha = "clip",
+    groups = {cracky=1}
+})
+
+local worklight_box = {
+	type = "fixed",
+	fixed = {
+		{-0.3750, -0.5000, 0.5000, 0.3750, -0.125, 0.1250}
+	}
+}
+
+minetest.register_node("sotm_nodes:worklight", {
+    description = "Work light",
+    drawtype = "mesh",
+    mesh = "sotm_worklight.obj",
+    tiles = {"sotm_worklight.png"},
+    groups = {metal=1, snappy=1, oddly_breakable_by_hand=1},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    light_source = minetest.LIGHT_MAX,
+    selection_box = worklight_box,
+    collision_box = worklight_box,
+})
+
 
 -- TODO: sublimation of water
 -- TODO: ladder, torch etc placement code to decide on vacuum or air surround in
--- mods/sotm_player/init.lua
+++ mods/sotm_player/init.lua
@@ -1,10 +1,19 @@
 -- Hand
+--[[
 minetest.register_tool(":", {
     wield_image = "sotm_hand.png",
     wield_scale = {x=1,y=1.5,z=4},
 	range = 3,
 	groups = {not_in_creative_inventory = 1},
+    tool_capabilities = {
+        full_punch_interval = 1.0,
+        max_drop_level = 1,
+        groupcaps = {
+            oddly_breakable_by_hand = {max_level=3},
+        }
+    },
 })
+--]]
 
 -- Spacesuit texture
 minetest.register_on_joinplayer(function(player)
